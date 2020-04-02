9c916e99fbed5e80d8e6ae9e529fcff791833620
chandatm <shandatm@gmail.com>
2020-04-02 14:11:30 -0500

    Update RegisterValidate.php

---
+++ RegisterValidate.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// validate fields
+
+//check if email arleady in database
+
+
+//if is - redirect to login page with error mesaage saying user already exsist
+
+
+//if not add to database and redirect to login
+
+
+
+
+
+
+
+
+?>
